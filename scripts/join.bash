509fed47c3ddd517332aebfaf7e82e6c6dc26ac0
brucechanjianle <jianle001@e.ntu.edu.sg>
2020-09-23 23:01:09 +0800

    add join script

---
+++ scripts/join.bash
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# This script joins this terminal to specific docker container
+
+# Display all container's name
+echo "List of containers:"
+declare -a arr
+i=0
+
+# Make container name into an array
+containers=$(docker ps -a | awk '{if(NR>1) print$NF}')
+for container in $containers
+do
+    arr[$i]=$container
+    let "i+=1"
+done
+
+# Loop through name array
+let "i-=1"
+for j in $(seq 0 $i)
+do
+    echo $j")" ${arr[$j]}
+done
+
+# Obtain container name
+read -p "Container name to be connected: " CONTAINERNAME
+
+if [[ -z ${arr[$CONTAINERNAME]} ]]
+then
+    docker exec --privileged -e DISPLAY=${DISPLAY} -e COLUMNS=`tput cols` -e LINES=`tput lines` -ti $CONTAINERNAME bash
+else
+    docker exec --privileged -e DISPLAY=${DISPLAY} -e COLUMNS=`tput cols` -e LINES=`tput lines` -ti ${arr[$CONTAINERNAME]} bash
+fi
