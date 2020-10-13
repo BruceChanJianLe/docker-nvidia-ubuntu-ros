687690d638a0f7ebf16301e96fecadf7af831e16
brucechanjianle <jianle001@e.ntu.edu.sg>
2020-10-13 09:01:00 +0800

    feat: build ubuntu version 20

--- scripts/build.bash
+++ scripts/build.bash
@@ -1,12 +1,36 @@
 #!/usr/bin/env bash
 # This script builds the docker image
 
-read -p "This script will build docker image ubuntu18.04:nvros continue[Y/n]?" value
+# Obtain Ubuntu version
+read -p "Select the Ubuntu version you wish to build upon[18/20]:" version
 
-if [[ -z $value || $value == y || $value == Y ]]
+if [[ $version == 18 ]]
 then
-    user_id=$(id -u)
-    docker build --rm -t "ubuntu18.04:nvros" --build-arg user_id=$user_id -f ../docker_build/Dockerfile .
+    # To build or not to build
+    read -p "This script will build docker image ubuntu18.04:nvros continue[Y/n]?" value
+
+    # Verify build check
+    if [[ -z $value || $value == y || $value == Y ]]
+    then
+        user_id=$(id -u)
+        docker build --rm -t "ubuntu18.04:nvros" --build-arg user_id=$user_id -f ../docker_build/u18/Dockerfile .
+    else
+        exit 1
+    fi
+elif [[ $version == 20 ]]
+then
+    # To build or not to build
+    read -p "This script will build docker image ubuntu20.04:nvros contunue[Y/n]?" value
+
+    # Verify build check
+    if [[ -z $value || $value == y || $value == Y ]]
+    then
+        user_id=$(id -u)
+        docker build --rm -t ubuntu20.04:nvros --build-arg user_id=$user_id -f ../docker_build/u20/Dockerfile .
+    else
+        exit 1
+    fi
 else
-    exit 1
+    # Ubuntu version not supported
+    echo -e "The Ubuntu version you have selected "$version" is currently not supported."
 fi
