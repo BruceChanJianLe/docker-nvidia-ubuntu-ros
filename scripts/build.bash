261fb1342a800eaf997929489c1c2c97a2387e98
chanjl <chanjl@i2r.a-star.edu.sg>
2022-01-25 20:19:24 +0800

    feat: update build script to build cuda GL u18

--- scripts/build.bash
+++ scripts/build.bash
@@ -6,16 +6,26 @@ read -p "Select the Ubuntu version you wish to build upon[18/20]:" version
 
 if [[ $version == 18 ]]
 then
-    # To build or not to build
-    read -p "This script will build docker image ubuntu18.04:nvros continue[Y/n]? " value
+    # Cuda selection
+    read -p "Do you want to have cuda for your Ubuntu18.04?[Y/n]" value
 
-    # Verify build check
+    # Verify cuda check
     if [[ -z $value || $value == y || $value == Y ]]
     then
         user_id=$(id -u)
-        docker build --rm -t "ubuntu18.04:nvros" --build-arg user_id=$user_id -f ../docker_build/u18/Dockerfile .
+        docker build --rm -t "ubuntu18.04:cnvros" --build-arg user_id=$user_id -f ../docker_build/u18/cuda/Dockerfile .
     else
-        exit 1
+        # To build or not to build
+        read -p "This script will build docker image ubuntu18.04:nvros continue[Y/n]? " value
+
+        # Verify build check
+        if [[ -z $value || $value == y || $value == Y ]]
+        then
+            user_id=$(id -u)
+            docker build --rm -t "ubuntu18.04:nvros" --build-arg user_id=$user_id -f ../docker_build/u18/Dockerfile .
+        else
+            exit 1
+        fi
     fi
 elif [[ $version == 20 ]]
 then
